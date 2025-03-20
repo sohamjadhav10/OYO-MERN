0966326e91d8fda86dbf2a32610f91e69d8c3561
Soham Jadhav <130537945+sohamjadhav10@users.noreply.github.com>
2025-03-20 15:04:57 +0530

    oyo

--- Server/app.js
+++ Server/app.js
@@ -17,12 +17,12 @@ app.use((req, res, next)=>{
 });
 
 
-mongoose.connect('mongodb://127.0.0.1:27017', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('✅ MongoDB Connected Successfully'))
-.catch(err => console.error('❌ MongoDB Connection Error:', err));
+// mongoose.connect('mongodb://127.0.0.1:27017', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+// .then(() => console.log('✅ MongoDB Connected Successfully'))
+// .catch(err => console.error('❌ MongoDB Connection Error:', err));
 
 //npm i cors
 app.use('/',routes);
