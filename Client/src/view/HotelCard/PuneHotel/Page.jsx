113739edea4c060c638116f765646cc1ccb9372c
Soham Jadhav <130537945+sohamjadhav10@users.noreply.github.com>
2025-03-21 02:16:23 +0530

    oyo

--- Client/src/view/HotelCard/PuneHotel/Page.jsx
+++ Client/src/view/HotelCard/PuneHotel/Page.jsx
@@ -1,29 +1,3 @@
-// import React, { Component } from "react";
-// import JSON1 from "./Pune.json"; 
-// import HotCard from "./HotCard";
-
- 
-
-// class Page extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       title: "Hotels in Pune",
-//       hotels: JSON1
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "20px",position:"relative",top:"100px" }}>
-//         <h1>{this.state.title}</h1>
-//         <HotCard hotList={this.state.hotels} /> 
-//       </div>
-//     );
-//   }
-// }
-
-// export default Page;
 
 import React, { Component } from "react";
 import axios from "axios";
